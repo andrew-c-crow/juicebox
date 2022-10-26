c7478b252f6b2f64e3d94477d43cf7983158093d
Andrew Crow <andrewccrow@gmail.com>
2022-10-26 12:33:05 -0500

    we can now access the data from the database via seed.js

--- db/index.js
+++ db/index.js
@@ -1,1 +1,7 @@
-a
+const { Client } = require('pg');
+
+const client = new Client('postgres://localhost:5432/juicebox-dev');
+
+module.exports = {
+  client,
+}
---
+++ db/seed.js
@@ -0,0 +1,22 @@
+// grab our client with destructuring from the export in index.js
+const { client } = require('./index');
+
+async function testDB() {
+  try {
+    // connect the client to the database, finally
+    client.connect();
+
+    // queries are promises, so we can await them
+    const {rows} = await client.query(`SELECT * FROM users;`);
+
+    // for now, logging is a fine way to see what's up
+    console.log(rows);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    // it's important to close out the client connection
+    client.end();
+  }
+}
+
+testDB();
