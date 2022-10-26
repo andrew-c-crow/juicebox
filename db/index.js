4d62b4ba08fc10f73e1edda292451570857a057a
Grant <grant.mc.foster@gmail.com>
2022-10-26 13:26:49 -0500

    created functions for table handling in seed.js

--- db/index.js
+++ db/index.js
@@ -2,8 +2,18 @@ const { Client } = require('pg');
 
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
-module.exports = {
-  client,
+
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username
+    FROM users;`
+  );
+
+  return rows;
 }
 
-test!!!
+//and export them
+module.exports = {
+  client,
+  getAllUsers
+}
--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,8 @@
 // grab our client with destructuring from the export in index.js
-const { client } = require('./index');
+const { client,
+getAllUsers
+ } = require('./index');
+
 
 async function testDB() {
   try {
@@ -7,16 +10,53 @@ async function testDB() {
     client.connect();
 
     // queries are promises, so we can await them
-    const {rows} = await client.query(`SELECT * FROM users;`);
+    const users = await getAllUsers();
 
     // for now, logging is a fine way to see what's up
-    console.log(rows);
+    console.log(users);
   } catch (error) {
     console.error(error);
-  } finally {
-    // it's important to close out the client connection
-    client.end();
   }
 }
 
-testDB();
+// this function should call a query which drops all tables from our database
+async function dropTables() {
+  try {
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `)
+  } catch (error) {
+    throw error; // we pass the error up to the function that calls dropTables
+  }
+}
+
+// this function should call a query which creates all tables for our database 
+async function createTables() {
+  try {
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username varchar(255) UNIQUE NOT NULL,
+      password varchar(255) NOT NULL
+    );
+    `);
+  } catch (error) {
+    throw error; // we pass the error up to the function that calls createTables
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+rebuildDB()
+.then(testDB)
+.catch(console.error)
+.finally(() => client.end() );
