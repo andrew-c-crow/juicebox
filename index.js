b37aca4c1da39566b5b0f81cbf3195d4c5ae95f4
Grant <grant.mc.foster@gmail.com>
2022-10-31 10:00:40 -0500

    set up router for api, started users route

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const server = express();
+const PORT = 3000;
+const apiRouter = require('./api');
+const morgan = require('morgan');
+const { client } = require('./db');
+
+
+server.use(morgan('dev'));
+
+server.use(express.json())
+
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
+
+server.use('/api', apiRouter);
+
+client.connect();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
