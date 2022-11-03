1fd98ea289796b1fba84a7cf8bc4608a32dd8ccc
Grant <grant.mc.foster@gmail.com>
2022-11-03 11:13:05 -0500

    fixed issue with tags route, should be functional now

--- api/tags.js
+++ api/tags.js
@@ -23,18 +23,20 @@ tagsRouter.get('/:tagName/posts', async (req, res, next) => {
   try {
     // use our method to get posts by tag name from the db
     const postByTagName = await getPostsByTagName(tagName)
+    console.log("posts by filtered tag here!!",postByTagName)
     const posts = postByTagName.filter(post => {
-      if (post.active && post.author.id !== req.user.id) {
-        return true;
-      }
-      // if (req.user && post.author.id !== req.user.id) {
+      // if (post.active) {
       //   return true;
       // }
+      if (post.active && req.user && req.user.id !== post.author.id) {
+        return true;
+      }
       return false;
     })
 
     // send out an object to the client { posts: // the posts }
-    res.send({ post: posts})
+    // res.send({ post: posts})
+    res.send({ posts })
   } catch ({ name, message }) {
     // forward the name and message to the error handler
     next({ name, message });
